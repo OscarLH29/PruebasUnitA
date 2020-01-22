081bc7f80e26583833a8d61954864987ef7d39ca
OscarLH29 <oscar_lopezh29@outlook.com>
2020-01-21 20:32:58 -0600

    returnValue

--- src/app/intermedias/espias/medicos.component.spec.ts
+++ src/app/intermedias/espias/medicos.component.spec.ts
@@ -1,6 +1,6 @@
 import { MedicosComponent } from './medicos.component';
 import { MedicosService } from './medicos.service';
-import { from, of } from 'rxjs';
+import {EMPTY, empty, from, Observable, of, throwError} from 'rxjs';
 
 
 describe('MedicosComponent', () => {
@@ -27,5 +27,60 @@ describe('MedicosComponent', () => {
 
     });
 
+    it('debe llamar al servidor para agregar medico', () => {
+        const espia = spyOn( servicio, 'agregarMedico' ).and.callFake( medico => {
+          return EMPTY;
+        });
+
+        componente.agregarMedico();
+
+        expect( espia ).toHaveBeenCalled();
+    });
+
+
+    it('Debe agregar nuevo medico al arreglo', () => {
+        const medico = {
+          id: 1,
+          nombre: 'Juan'
+        };
+
+        spyOn( servicio, 'agregarMedico' ).and.returnValue( from( [medico] ));
+
+        componente.agregarMedico();
+
+        expect( componente.medicos.indexOf(medico) ).toBeGreaterThanOrEqual(0);
+    });
+
+    it('si falla se debe mostrar el mensaje de error del servicio', () => {
+        const miError = 'Error: no se pudo agregar';
+
+        spyOn(servicio, 'agregarMedico' ).and.returnValue(
+          throwError(miError)
+        );
+
+        componente.agregarMedico();
+
+        expect(componente.mensajeError).toBe( miError );
+    });
+
+    it('debe llamar al servidor para borrar medico', () => {
+        spyOn(window, 'confirm').and.returnValue(true);
+        const espia = spyOn( servicio, 'borrarMedico' ).and.returnValue( EMPTY );
+
+        componente.borrarMedico('1');
+
+        expect( espia ).toHaveBeenCalledWith('1');
+
+    });
+
+  it('No debe llamar al servidor para borrar medico', () => {
+    spyOn(window, 'confirm').and.returnValue(false);
+    const espia = spyOn( servicio, 'borrarMedico' ).and.returnValue( EMPTY );
+
+    componente.borrarMedico('1');
+
+    expect( espia ).not.toHaveBeenCalledWith('1');
+
+  });
 
 });
